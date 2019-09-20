1fc773f98ce08a8731ae7aa499be56503621c091
Matheus Joranhezon <mjoranhezon@gmail.com>
2019-09-20 17:21:29 -0300

    Updating route to create user

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from project.api.routes.auth import auth_blueprint
 
 # instantiate the app
 app = Flask(__name__)
@@ -7,6 +8,8 @@ app = Flask(__name__)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+app.register_blueprint(auth_blueprint)
 
 
 
--- project/api/routes/auth.py
+++ project/api/routes/auth.py
@@ -10,15 +10,18 @@ auth_blueprint = Blueprint('auth_blueprint', __name__)
 """
 Nickname, email, password, password confirmation
 """
-@auth_blueprint.route('/api/sign-up-gateway', methods=['POST'])
+@auth_blueprint.route('/sign-up-gateway', methods=['POST'])
 def sign_up_gateway():
     user_data = request.get_json()
-    url = "localhost:5000/api/sign-up"
+    print(user_data)
+    print(1)
+    url = "http://localhost:5001/sign-up"
 
     if user_data is not None:
-        sign_up_request = requests.request("POST", url, user_data,
-                                           headers = {'Accept': 'application/json', 'content-type' : 'application/json'})
+        print('oi')
+        sign_up_request = requests.request("POST", url, data = user_data)
 
+        print(sign_up_request)
         if sign_up_request['status'] is 200:
             return jsonify({
                 'message': 'User account was created successfully' 
@@ -27,4 +30,9 @@ def sign_up_gateway():
             return jsonify({
                 'message': 'Could not create user account'
             }), 400
+    else:
+        print(2)
+        return jsonify({
+            'message': 'Could not reach Player server'
+        }), 400
     
