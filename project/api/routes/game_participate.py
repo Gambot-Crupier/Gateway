1621e85644c605d15ff3614d019a4fbefe905657
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-10-15 23:56:07 -0300

    HotFix

--- project/api/routes/game_participate.py
+++ project/api/routes/game_participate.py
@@ -6,7 +6,6 @@ import requests, os, json, sys
 player_in_game_blueprint = Blueprint('player_in_game_blueprint', __name__)
 base_url = os.getenv('GAMBOT_GAME_URL')
 
-
 @player_in_game_blueprint.route('/post_player_in_game', methods=['POST'])
 def post_game_participate():
     player_in_game_data = request.get_json()
@@ -32,12 +31,11 @@ def post_game_participate():
 @player_in_game_blueprint.route('/get_players_in_game', methods=['GET'])
 def get_game_participate():
     url = base_url + "get_players_in_game"
-    PARAMS = {"game_id": request.args.get('game_id')}
-    
-    post_player_in_request = requests.request("GET", url, params=PARAMS)
 
-    if post_player_in_request.status_code is 200:
-        return jsonify(post_player_in_request.json()), 200
+    get_players_request = requests.request("GET", url)
+
+    if get_players_request.status_code is 200:
+        return jsonify(get_players_request.json()), 200
     else:
         return jsonify({
             'message': 'Could not get players in game'
