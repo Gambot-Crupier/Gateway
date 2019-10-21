b2c9a14f16e9dbf09dafba4304c2862d6ea14bb4
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-10-21 20:49:15 -0300

    Adds GetPlayersInGame

--- project/api/routes/game_participate.py
+++ project/api/routes/game_participate.py
@@ -4,12 +4,13 @@ from flask import request
 import requests, os, json, sys
 
 player_in_game_blueprint = Blueprint('player_in_game_blueprint', __name__)
-base_url = os.getenv('GAMBOT_GAME_URL')
+base_game_url = os.getenv('GAMBOT_GAME_URL')
+base_player_url = os.getenv('GAMBOT_PLAYER_URL')
 
 @player_in_game_blueprint.route('/post_player_in_game', methods=['POST'])
 def post_game_participate():
     player_in_game_data = request.get_json()
-    url = base_url + "post_player_in_game"
+    url = base_game_url + "post_player_in_game"
 
     if player_in_game_data is not None:
         post_player_in_request = requests.post(url, json = json.dumps(player_in_game_data))
@@ -30,13 +31,23 @@ def post_game_participate():
 
 @player_in_game_blueprint.route('/get_players_in_game', methods=['GET'])
 def get_game_participate():
-    url = base_url + "get_players_in_game"
+    game_url = base_game_url + "get_players_in_game"
+    player_url = base_player_url + "get_user_by_id"
 
-    get_players_request = requests.request("GET", url)
+    get_players_id_request = requests.request("GET", game_url)
+    
+    if get_players_id_request.status_code is 200:
+        players_in_game = get_players_id_request.json()
 
-    if get_players_request.status_code is 200:
-        return jsonify(get_players_request.json()), 200
+
+        for index, player in enumerate(players_in_game['players']):
+            get_player_request = requests.request("GET", player_url, params={"user_id": player['player_id']})
+            
+            if get_player_request.status_code is 200:
+                players_in_game['players'][index] = get_player_request.json()
+            else:
+                return jsonify({'message': 'Could not get player with Id ' + player['player_id']}), 400
+
+        return jsonify(players_in_game), 200 
     else:
-        return jsonify({
-            'message': 'Could not get players in game'
-        }), 400
+        return jsonify({'message': 'Could not get players in game'}), 400
