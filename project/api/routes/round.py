5db75c747980c8b31c386ed1dbea9a801e88106b
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-11-13 15:22:37 -0300

    Adds routes
    get_player_bet, get_round_bet, create_round

--- project/api/routes/round.py
+++ project/api/routes/round.py
@@ -7,19 +7,55 @@ round_blueprint = Blueprint('round_blueprint', __name__)
 base_game_url = os.getenv('GAMBOT_GAME_URL')
 
 
-@round_blueprint.route('/get_player_money', methods=['GET'])
-def get_player_money():
+@round_blueprint.route('/create_round', methods=['POST'])
+def create_round():
+    url = base_game_url + "create_round"
+
     try:
-        url = base_game_url + "get_player_money"
-        params={"player_id": request.args.get('player_id'), "game_id": request.args.get('game_id')}
-
-        get_player_money_request = requests.request("GET", url, params=params)
-
-
-        if get_player_money_request.status_code is 200:
-            return jsonify(get_player_money_request.json()), 200
-        else:
-            return jsonify({ 'message': get_player_money_request.json()['message'] }), 406
+        create_round_request = requests.post(url)
+        return jsonify(create_round_request.json()), create_round_request.status_code
 
     except Exception as e:
-        return jsonify({"error": "Error on starting Game", "message": str(e)}), 500
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+
+@round_blueprint.route('/get_round_bet', methods=['GET'])
+def get_round_bet():
+    url = base_game_url + "get_round_bet"
+    params = {"round_id": request.args.get('round_id')}
+
+    try:
+        get_round_bet_request = requests.request("GET", url, params=params)
+        return jsonify(get_round_bet_request.json()), get_round_bet_request.status_code
+
+    except Exception as e:
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+
+@round_blueprint.route('/get_player_money', methods=['GET'])
+def get_player_money():
+    url = base_game_url + "get_player_money"
+    params = {"game_id": request.args.get('game_id'), "player_id": request.args.get('player_id')}
+
+    try:
+        get_player_money_request = requests.request("GET", url, params=params)
+        return jsonify(get_player_money_request.json()), get_player_money_request.status_code
+
+    except Exception as e:
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+
+@round_blueprint.route('/get_player_bet', methods=['GET'])
+def get_player_bet():
+    url = base_game_url + "get_player_bet"
+    params = {"game_id": request.args.get('game_id'), "player_id": request.args.get('player_id')}
+
+    try:
+        get_player_bet_request = requests.request("GET", url, params=params)
+        return jsonify(get_player_bet_request.json()), get_player_bet_request.status_code
+
+    except Exception as e:
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
