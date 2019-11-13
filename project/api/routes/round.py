4033a3c2acb2c6be4fcd957973d86bcbc5575de0
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-11-13 15:29:13 -0300

    Adds routes
    raise_bet, pay_bet, leave_match

--- project/api/routes/round.py
+++ project/api/routes/round.py
@@ -59,3 +59,47 @@ def get_player_bet():
 
     except Exception as e:
         return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+
+@round_blueprint.route('/pay_bet', methods=['POST'])
+def pay_bet():
+    url = base_game_url + "pay_bet"
+    params = {"game_id": request.args.get('game_id'), "player_id": request.args.get('player_id'), 
+                "round_id": request.args.get('round_id')}
+
+    try:
+        pay_bet_request = requests.request("POST", url, params=params)
+        return jsonify(pay_bet_request.json()), pay_bet_request.status_code
+
+    except Exception as e:
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+
+@round_blueprint.route('/raise_bet', methods=['POST'])
+def raise_bet():
+    url = base_game_url + "raise_bet"
+    params = {"game_id": request.args.get('game_id'), "player_id": request.args.get('player_id'), 
+                "round_id": request.args.get('round_id'), "value": request.args.get('value')}
+
+    try:
+        raise_bet_request = requests.request("POST", url, params=params)
+        return jsonify(raise_bet_request.json()), raise_bet_request.status_code
+
+    except Exception as e:
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+
+@round_blueprint.route('/leave_match', methods=['POST'])
+def leave_match():
+    url = base_game_url + "leave_match"
+    params = {"game_id": request.args.get('game_id'), "player_id": request.args.get('player_id')}
+
+    try:
+        leave_match_request = requests.request("POST", url, params=params)
+        return jsonify(leave_match_request.json()), leave_match_request.status_code
+
+    except Exception as e:
+        return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
