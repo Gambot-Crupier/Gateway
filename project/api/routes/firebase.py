62d47899c24c87ea16a2f30d483afb5ea4d8de0b
Matheus Joranhezon <mjoranhezon@gmail.com>
2019-11-22 15:16:05 -0200

    Change topic firebase message to individual message

--- project/api/routes/firebase.py
+++ project/api/routes/firebase.py
@@ -7,8 +7,10 @@ def create_topic(players_list, game_id):
     return response
 
 def message_app(data, game_id):
-    topic = 'Gambot'
-    message = messaging.Message(topic = topic, data = data)
-    response = messaging.send(message)
+    players_list = PlayerInGame.query.filter_by(game_id = game_id).all()
     
+    for player in players_list:
+        message = messaging.Message(data = data, token = player.device_id)
+        response = messaging.send(message)
+
     return response
