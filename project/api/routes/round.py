6af6ca283ea5b34f1ddd19543440b82ce3e458e3
Marina Joranhezon <mjoranhezon@gmail.com>
2019-11-28 17:02:26 -0200

    Fix bugs

--- project/api/routes/round.py
+++ project/api/routes/round.py
@@ -8,14 +8,15 @@ base_game_url = os.getenv('GAMBOT_GAME_URL')
 base_player_url = os.getenv('GAMBOT_PLAYER_URL')
 
 @round_blueprint.route('/get_round_id', methods=['GET'])
-def get_round_id:
+def get_round_id():
     game_url = base_game_url + 'get_round_id'
 
     response = requests.request("GET", game_url)
+    data = response.json()
 
     if response.status_code is 200:
         return jsonify({
-            "round_id": response.round_id
+            "round_id": data['round_id']
         }), 200
     else:
         return jsonify({
@@ -72,7 +73,24 @@ def get_round_bet():
     except Exception as e:
         return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
 
-
+@round_blueprint.route('/round_redirect', methods=['POST'])
+def round_redirect():
+    url = base_game_url + 'round_redirect'
+    
+    try:
+        request = requests.request("POST", url)
+        if request.status_code == 200:
+            return jsonify({
+                'message': 'boooom'
+            }), 200
+        else:
+            return jsonify({
+                'message': request.json()
+            }), 400
+    except Exception as e:
+        return jsonify({
+            'message': 'deu ruim'
+        }), 400
 
 @round_blueprint.route('/get_player_money', methods=['GET'])
 def get_player_money():
@@ -143,3 +161,19 @@ def leave_match():
 
     except Exception as e:
         return jsonify({"error": "Error on Creating Round", "message": str(e)}), 502
+
+
+@round_blueprint.route('/get_current_player', methods=['GET'])
+def get_current_player():
+    url = base_game_url + 'get_current_player'
+    params = {'round_id': request.args.get('round_id')}
+
+    try:
+        current_player_request = requests.request("GET", url, params=params)
+        return jsonify(
+            current_player_request.json()
+        ), current_player_request.status_code
+    except Exception as e:
+        return jsonify({
+            "message": str(e)
+        }), 400
