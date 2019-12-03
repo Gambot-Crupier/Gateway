f6ecaf7ae172181c48636535f9f803d373e5b93e
Bruno Dantas <brunodantas@localhost.localdomain>
2019-12-03 13:18:17 -0300

    Configuraçoes de deploy

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from flask.cli import FlaskGroup
 from project import app
+from flask import jsonify
 
 
 cli = FlaskGroup(app)
 
+@app.route('/')
+def hello_world():
+    return jsonify({"message": "Hello World!"})
 
 if __name__ == '__main__':
     cli()
